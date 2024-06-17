4bee2d8f8fea310260fc09314460804fe00069f9
SomeRadGuy <muhammmad.sanaullah1956@gmail.com>
2024-06-18 04:00:33 +0500

    Skills page added

--- src/components/Skills/skills.js
+++ src/components/Skills/skills.js
@@ -8,9 +8,38 @@ const header = () => {
         <div className="skills_header_div">
           <h1>Skills</h1>
         </div>
-        <div className="skills_body">
-          <div className="left_skills_div"></div>
-          <div className="right_skills_div"></div>
+
+        <div className="skills_top_row skill_rows">
+          <div className="one_skill_div">
+            <h4>Photoshop - 100%</h4>
+            <div className="skills_bar hundred_bar"></div>
+          </div>
+          <div className="one_skill_div">
+            <h4>Illustrator - 100%</h4>
+            <div className="skills_bar hundred_bar"></div>
+          </div>
+        </div>
+
+        <div className="skills_middle_row skill_rows">
+          <div className="one_skill_div">
+            <h4>Python - 70%</h4>
+            <div className="skills_bar seventy_bar"></div>
+          </div>
+          <div className="one_skill_div">
+            <h4>React.js - 50%</h4>
+            <div className="skills_bar fifty_bar"></div>
+          </div>
+        </div>
+
+        <div className="skills_bottom_row skill_rows">
+          <div className="one_skill_div">
+            <h4>HTML 5 - 70%</h4>
+            <div className="skills_bar seventy_bar"></div>
+          </div>
+          <div className="one_skill_div">
+            <h4>CSS 3 - 70%</h4>
+            <div className="skills_bar seventy_bar"></div>
+          </div>
         </div>
       </div>
     </>
